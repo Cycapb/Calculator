b46e2603090d76a925d65b937e75e1636b7faca9
sav <sav@SAV>
2016-10-13 16:15:42 +0500

    Created red test ConvertReturnsNullIfInputStringIsNullOrEmpty. Created types IValidator, PostfixConverter

---
+++ Calculator/BusinessLogic/PostfixConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace BusinessLogic
+{
+    public class PostfixConverter:IConverter
+    {
+        private readonly IValidator _validator;
+
+        public PostfixConverter(IValidator validator)
+        {
+            _validator = validator;
+        }
+
+
+        public string Convert(string infixString)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/Tests/ConverterTests.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Tests
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        private readonly PostfixConverter _converter;
+        private readonly Mock<IValidator> _validator;
+
+        public ConverterTests()
+        {
+            _validator = new Mock<IValidator>();
+            _converter = new PostfixConverter(_validator.Object);
+        }
+
+        [TestMethod]
+        public void ConvertReturnsNullIfInputStringIsNullOrEmpty()
+        {
+            var result = _converter.Convert("");
+            
+            Assert.IsNull(result);
+        }
+    }
+}
