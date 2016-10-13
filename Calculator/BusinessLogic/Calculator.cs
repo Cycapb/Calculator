408417606745da028d95d1ca01d2d14dbdb14d72
Apostol <Apostol@MAIN>
2016-10-13 14:01:22 +0500

    Created types IConverter and IExecutor. Injected them into Calculator's constructor.

--- Calculator/BusinessLogic/Calculator.cs
+++ Calculator/BusinessLogic/Calculator.cs
@@ -2,6 +2,15 @@
 {
     public class Calculator
     {
+        private readonly IConverter _converter;
+        private readonly IExecuter _executer;
+
+        public Calculator(IConverter converter, IExecuter executer)
+        {
+            _converter = converter;
+            _executer = executer;
+        }
+
         public int Calculate(string infixString)
         {
             throw new System.NotImplementedException();
--- Calculator/Tests/CalculatorTests.cs
+++ Calculator/Tests/CalculatorTests.cs
@@ -1,18 +1,23 @@
 ﻿using BusinessLogic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Tests
 {
     [TestClass]
     public class CalculatorTests
     {
+        private readonly Mock<IConverter> _converter;
+        private readonly Mock<IExecuter> _executer;
+        private readonly Calculator _calculator;
+
         [TestMethod]
         public void CanCalculate()
         {
-            var infixString = "2+2";
-            var calculator = new Calculator();
 
-            var result = calculator.Calculate(infixString);
+            var infixString = "2+2";
+            
+            var result = _calculator.Calculate(infixString);
 
             Assert.AreEqual(4, result);
         }
