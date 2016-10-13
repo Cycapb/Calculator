2eae68de36a8f307524a284eae5d6312fefa2a93
Apostol <Apostol@MAIN>
2016-10-13 14:13:26 +0500

    Added methods into IExecutor и IConverter. Began realization of red test CanCalculate.

--- Calculator/BusinessLogic/IExecuter.cs
+++ Calculator/BusinessLogic/IExecuter.cs
@@ -2,6 +2,6 @@
 {
     public interface IExecuter
     {
-        
+        decimal Execute(string postfixString);
     }
 }
--- Calculator/Tests/CalculatorTests.cs
+++ Calculator/Tests/CalculatorTests.cs
@@ -11,15 +11,22 @@ namespace Tests
         private readonly Mock<IExecuter> _executer;
         private readonly Calculator _calculator;
 
+        public CalculatorTests()
+        {
+            _converter = new Mock<IConverter>();
+            _executer = new Mock<IExecuter>();
+            _calculator = new Calculator(_converter.Object,_executer.Object);
+        }
+
+
         [TestMethod]
         public void CanCalculate()
         {
+            _converter.Setup(m => m.Convert(It.IsAny<string>())).Returns(It.IsNotNull<string>());
 
-            var infixString = "2+2";
-            
-            var result = _calculator.Calculate(infixString);
+            _calculator.Calculate("");
 
-            Assert.AreEqual(4, result);
+            _executer.Verify(m => m.Execute(It.IsAny<string>()),Times.Exactly(1));
         }
     }
 }
