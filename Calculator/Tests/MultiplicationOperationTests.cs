72333b290d7d27810aed487d95d96830219a6c3e
Apostol <Apostol@MAIN>
2016-10-14 15:43:32 +0500

    Created multiplicationOperation test unit. Added red test method Execute2Multiplicate.

---
+++ Calculator/Tests/MultiplicationOperationTests.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogic.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class MultiplicationOperationTests
+    {
+        private readonly MultiplicationOperation _multiplicationOperation;
+
+        public MultiplicationOperationTests()
+        {
+            _multiplicationOperation = new MultiplicationOperation();
+        }
+
+        [TestMethod]
+        public void Execute2Multiplicate2()
+        {
+            var x = 2;
+            var y = 2;
+            var result = _multiplicationOperation.Execute(x, y);
+
+            Assert.AreEqual(4, result);
+        }
+    }
+}
