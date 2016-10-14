18ef44d319b990839019e8c811bf3341d8b9ad11
Apostol <Apostol@MAIN>
2016-10-14 13:57:35 +0500

    Created test class OperationProviderTests. Added test method IsOperationInputPlusReturnsTrue. Created type SimpleOperationProvider.

---
+++ Calculator/BusinessLogic/SimpleOperationProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class SimpleOperationProvider:IOperationProvider
+    {
+        private readonly IDictionary<char, IOperation> _operations;
+
+        public SimpleOperationProvider()
+        {
+                
+        }
+
+        public bool IsOperation(char c)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IOperation GetOperation(char c)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/Tests/OperationProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class OperationProviderTests
+    {
+        private readonly SimpleOperationProvider _operationProvider;
+
+        public OperationProviderTests()
+        {
+            _operationProvider = new SimpleOperationProvider();
+        }
+
+        [TestMethod]
+        public void IsOperationInputPlusReturnsTrue()
+        {
+            var operation = '+';
+
+            var result = _operationProvider.IsOperation(operation);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
