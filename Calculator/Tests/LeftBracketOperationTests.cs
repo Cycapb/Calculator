10c1979bb78f6189cc9aba4692dfe92c7ec9d919
Apostol <Apostol@MAIN>
2016-10-14 15:33:32 +0500

    Created LeftBracketOperationTests unit test and added test method GetPriority. It successfully passes.

---
+++ Calculator/Tests/LeftBracketOperationTests.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class LeftBracketOperationTests
+    {
+        private readonly LeftBracketOperation _leftBracketOperation;
+
+        public LeftBracketOperationTests()
+        {
+            _leftBracketOperation = new LeftBracketOperation();
+        }
+
+        [TestMethod]
+        public void GetPriority()
+        {
+            var result = _leftBracketOperation.GetPriority;
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
