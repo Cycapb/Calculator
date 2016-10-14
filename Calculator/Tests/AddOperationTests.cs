0d382c46e61ae06aa7d8b958ab60961f6457c9b6
Apostol <Apostol@MAIN>
2016-10-14 15:04:32 +0500

    Added unit test AddOperationTests. Create red test CanExecute2Plus2

---
+++ Calculator/Tests/AddOperationTests.cs
@@ -0,0 +1,28 @@
+﻿using System.CodeDom;
+using BusinessLogic.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AddOperationTests
+    {
+        private readonly AddOperation _addOperation;
+
+        public AddOperationTests()
+        {
+            _addOperation = new AddOperation();
+        }
+
+        [TestMethod]
+        public void CanExecute2Plus2()
+        {
+            var x = 2;
+            var y = 2;
+
+            var result = _addOperation.Execute(x,y);
+
+            Assert.AreEqual(4, result);
+        }
+    }
+}
