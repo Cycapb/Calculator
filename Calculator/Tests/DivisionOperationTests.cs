cbf755b1ab6c5164ece2b6f0e7d0f8341068dfb4
Apostol <Apostol@MAIN>
2016-10-14 15:11:08 +0500

    Created DivisionOperationTests. Added red  test method CanExecute4Div2

---
+++ Calculator/Tests/DivisionOperationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BusinessLogic.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DivisionOperationTests
+    {
+        private readonly DivisionOperation _divOperation;
+
+        public DivisionOperationTests()
+        {
+            _divOperation = new DivisionOperation();
+        }
+
+        [TestMethod]
+        public void CanExecute4Div2()
+        {
+
+            var result = _divOperation.Execute(4, 2);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
