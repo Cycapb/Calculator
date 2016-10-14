672eec637b1877480e11612115663b875d2fa286
Apostol <Apostol@MAIN>
2016-10-14 18:43:22 +0500

    Implemented logic in SimpleExecuter. Test Execute2Plus2 now successfully passes

--- Calculator/BusinessLogic/SimpleExecuter.cs
+++ Calculator/BusinessLogic/SimpleExecuter.cs
@@ -1,10 +1,48 @@
-﻿namespace BusinessLogic
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic
 {
     public class SimpleExecuter:IExecuter
     {
+        private readonly IOperationProvider _operationProvider;
+
+        public SimpleExecuter(IOperationProvider operationProvider)
+        {
+            _operationProvider = operationProvider;
+        }
+
         public decimal Execute(string postfixString)
         {
-            throw new System.NotImplementedException();
+            decimal result = 0;
+            Stack<decimal> temp = new Stack<decimal>();
+
+            for (int i = 0; i < postfixString.Length; i++)
+            {
+                //Если цифра
+                if (char.IsDigit(postfixString[i]))
+                {
+                    string a = string.Empty;
+
+                    while (" ".IndexOf(postfixString[i]) == -1 && !_operationProvider.IsOperation(postfixString[i]))
+                    {
+                        a += postfixString[i]; 
+                        i++;
+                        if (i == postfixString.Length) break;
+                    }
+                    temp.Push(decimal.Parse(a)); 
+                    i--;
+                }
+                //Если оператор
+                else if (_operationProvider.IsOperation(postfixString[i])) 
+                {
+                    decimal a = temp.Pop();
+                    decimal b = temp.Pop();
+
+                    result = ((IOperationExecuter) _operationProvider.GetOperation(postfixString[i])).Execute(a, b);
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek();
         }
     }
 }
--- Calculator/Tests/ExecuterTests.cs
+++ Calculator/Tests/ExecuterTests.cs
@@ -1,18 +1,30 @@
 ﻿using BusinessLogic;
+using BusinessLogic.Operations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Tests
 {
     [TestClass]
     public class ExecuterTests
     {
+        private readonly Mock<IOperationProvider> _operationProvider;
+        private readonly SimpleExecuter _executor;
+
+        public ExecuterTests()
+        {
+            _operationProvider = new Mock<IOperationProvider>();
+            _executor = new SimpleExecuter(_operationProvider.Object);
+        }
+
         [TestMethod]
-        public void Execute()
+        public void Execute2Plus2()
         {
             var postfixString = "2 2 + ";
-            var executor = new SimpleExecuter();
+            _operationProvider.Setup(m => m.IsOperation('+')).Returns(true);
+            _operationProvider.Setup(m => m.GetOperation('+')).Returns(new AddOperation());
 
-            var result = executor.Execute(postfixString);
+            var result = _executor.Execute(postfixString);
 
             Assert.AreEqual(4, result);
         }
