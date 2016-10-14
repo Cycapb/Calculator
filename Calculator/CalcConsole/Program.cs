94ccf6b22ade0b02614b7b5e05590d41f5ccd3b2
Apostol <Apostol@MAIN>
2016-10-14 20:07:15 +0500

    Created console application.

--- Calculator/CalcConsole/Program.cs
+++ Calculator/CalcConsole/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace CalcConsole
+﻿using System;
+using BusinessLogic;
+using Ninject;
+
+namespace CalcConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Input expression:\n");
+            var exp = Console.ReadLine();
+            Console.WriteLine(Calculate(exp));
+            Console.ReadLine();
+        }
+
+        private static decimal Calculate(string inputString)
+        {
+            var kernel = GetKernel();
+            var calculator = kernel.Get<Calculator>();
+            return calculator.Calculate(inputString);
+        }
+
+        private static IKernel GetKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Calculator>().ToSelf();
+            kernel.Bind<IExecuter>().To<SimpleExecuter>();
+            kernel.Bind<IValidator>().To<InfixStringValidator>();
+            kernel.Bind<IOperationProvider>().To<SimpleOperationProvider>();
+            kernel.Bind<IConverter>().To<PostfixConverter>();
+            return kernel;
         }
     }
 }
