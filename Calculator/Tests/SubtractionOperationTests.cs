f395314040458241a2ac8840e4efdd4e2adc4789
Apostol <Apostol@MAIN>
2016-10-14 15:56:33 +0500

    Created unit test SubtractionOperationTests. Added red test Execute4Minus2Returns2.

---
+++ Calculator/Tests/SubtractionOperationTests.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class SubtractionOperationTests
+    {
+        private readonly SubtractionOperation _subtractionOperation;
+
+        public SubtractionOperationTests()
+        {
+            _subtractionOperation = new SubtractionOperation();
+        }
+
+        [TestMethod]
+        public void Execute4Minus2Returns2()
+        {
+            var x = 4;
+            var y = 2;
+
+            var result = _subtractionOperation.Execute(x, y);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
